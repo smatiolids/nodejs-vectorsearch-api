b07c27c8d4ab8451de9e910c51945755651e6b1d
Samuel Matioli <samuel.matioli@datastax.com>
2024-05-28 18:06:35 +0000

    Address Translator

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 const cassandra = require('cassandra-driver');
 const util = require('util')
+const AddressTranslator = require('cassandra-driver').policies.addressResolution.AddressTranslator;
 
-// function StaticAddressTranslator() {
-// }
-
-// util.inherits(StaticAddressTranslator, cassandra.AddressTranslator);
-
-// StaticAddressTranslator.prototype.translate = function (address, port, callback) {
-//     callback(null, "127.0.0.1","9042") 
-// };
+class MyAddressTranslator extends AddressTranslator {
+    translate(address, port, callback) {
+      callback(null, process.env.DSE_NODES, port)
+    }
+  }
 
 const client = new cassandra.Client({
     contactPoints: [process.env.DSE_NODES],
     localDataCenter: process.env.DSE_DC,
     keyspace: process.env.DSE_KEYSPACE,
     authProvider: new cassandra.auth.PlainTextAuthProvider(process.env.DSE_USER, process.env.DSE_PASS),
-    // policies: {
-    //     addressResolution: new StaticAddressTranslator()
-    // }
-
+    socketOptions: {
+        connectTimeout : 10000
+    }, 
+    policies: { 
+        addressResolution: new MyAddressTranslator() 
+      }
 });
 
 client.connect()
